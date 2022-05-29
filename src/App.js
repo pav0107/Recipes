5e8a5a62a2d855facecb7a577d71d758e9f0a130
Pav <pav0107@gmail.com>
2022-05-29 11:21:30 +0100

    add search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Pages from './pages/Pages';
 import Category from './components/Category';
 import { BrowserRouter } from 'react-router-dom';
+import Search from './components/Search';
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Category />
         <Pages />
       </BrowserRouter>
