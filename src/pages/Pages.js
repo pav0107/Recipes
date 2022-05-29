7668cb34cb262b827b196b6b8c71d1eefe74eaa0
Pav <pav0107@gmail.com>
2022-05-29 11:54:14 +0100

    hook up search functionality

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,5 +1,6 @@
 import Home from './Home';
 import Cuisine from './Cuisine';
+import Searched from './Searched';
 import { Route, Routes } from 'react-router-dom';
 
 const Pages = () => {
@@ -7,6 +8,7 @@ const Pages = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/cuisine/:type" element={<Cuisine />} />
+      <Route path="/searched/:search" element={<Searched />} />
     </Routes>
   );
 };
