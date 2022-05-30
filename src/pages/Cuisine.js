07f2a13862d4008749097758a168795411dc6fd3
Pav <pav0107@gmail.com>
2022-05-30 16:42:34 +0100

    add transitions

--- src/pages/Cuisine.js
+++ src/pages/Cuisine.js
@@ -20,7 +20,12 @@ const Cuisine = () => {
   }, [params.type]);
 
   return (
-    <Grid>
+    <Grid
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       {cuisine.map((item) => {
         console.log(cuisine);
         return (
@@ -36,7 +41,7 @@ const Cuisine = () => {
   );
 };
 
-const Grid = styled.div`
+const Grid = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
   grid-gap: 3rem;
