c63fe84a529433ab10e1fcb4ee5f4a04b48cd66a
Pav <pav0107@gmail.com>
2022-05-30 16:17:12 +0100

    add more links

--- src/pages/Searched.js
+++ src/pages/Searched.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Searched = () => {
   const [searchedRecipes, setSearchedRecipes] = useState([]);
@@ -23,8 +24,10 @@ const Searched = () => {
       {searchedRecipes.map((item) => {
         return (
           <Card key={item.id}>
-            <img src={item.image} alt="" />
-            <h4>{item.title}</h4>
+            <Link to={'/recipe/' + item.id}>
+              <img src={item.image} alt="" />
+              <h4>{item.title}</h4>
+            </Link>
           </Card>
         );
       })}
